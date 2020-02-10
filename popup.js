237619156a8aeab848df3f45b6c58077588409d1
Nathanael Gentry <ngentry1@liberty.edu>
2020-02-10 13:05:43 -0500

    update popup.js idiom

--- popup.js
+++ popup.js
@@ -1,15 +1,15 @@
 document.addEventListener('DOMContentLoaded', function () {
     let Browser;
     let server = "http://localhost:5000/";
+    var font_data;
 
-    // DETECT BROWSER TYPE FOR PROPER API USE
     try {
-        // FOR FIREFOX
+        // Firefox
         Browser = browser;
         document.getElementById("color").disabled = true;
     } catch(e) {
         try {
-            // FOR CHROME
+            // Chrome
             Browser = chrome;
 
         } catch(e) {
@@ -17,8 +17,8 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     }
 
-
-    // Populate the font selector from local fonts.json.
+    // Populate the font selector from server fonts
+    let font = document.getElementById("font");
     $(document).ready(function() {
         let font_selector = $('#font');
         font_selector.empty();
@@ -33,22 +33,22 @@ document.addEventListener('DOMContentLoaded', function () {
             document.getElementById("fontLabel").innerHTML = font_data[font.value]["formal"];
         })
     });
-    // Sets values of the options before the user
-    //+ has entered their own selections
-    //+ (which would then be stored in localStorage)
+
+    // Update the font dynamic name whenever the font selection changes.
+    font.addEventListener("input", function(){
+        document.getElementById("fontLabel").innerHTML = font_data[font.value]["formal"];
+    })
+
+    // Pull default values from localstorage or set them to global defaults.
     function setDefaultValue(id, optionType){
-        // Checks whether there is anything stored for that ID
         if(localStorage.getItem(id) === null){
             if(optionType === "rgb"){
-                // sets rgb value to 0 as default value
                 localStorage.setItem(id, 0);
             }
             else if (optionType === "fontSize"){
-                // sets DPI value to 180 (aka 18pt) if localStorage is empty for id = DPI
                 localStorage.setItem(id, "180");
             }
             else if (optionType === "fontStyle"){
-                // sets font to Latin Modern if localStorage is empty for id = font
                 localStorage.setItem(id, "computer-modern");
             }
         }
@@ -62,13 +62,10 @@ document.addEventListener('DOMContentLoaded', function () {
 
     // Keep option values/selections persistent after extension closes
     function persistentOptions(selectID) {
-        // Sets input equal to the current element of the id passed in
         var input = document.getElementById(selectID);
-        // When the input changes for the id passed in, the key value (the id) is set the value of the new input
         input.addEventListener('input', function () {
             localStorage.setItem(selectID, input.value);
         });
-        // Sets the value in the html with the localStorage value of the specified key/ID
         input.value = localStorage.getItem(selectID);
     }
 
@@ -78,18 +75,12 @@ document.addEventListener('DOMContentLoaded', function () {
     persistentOptions("DPI");
     persistentOptions("font");
 
-
-
     var input = document.getElementById("code");
 
     // Listens for a change of input in the text box
     input.addEventListener('input', function () {
-        // User's input is stored in the text box
-        //+ after they click away from the extension
-        //+ (assumes that localstorage is supported on the browser)
         localStorage.setItem("text", input.value);
 
-        // Alert to make the user aware that certain text will be removed in the php file
         function checkInput(text) {
             if (input.value.includes(text)) {
                 // Alerted is set to "yes" if the alert has already
@@ -97,11 +88,8 @@ document.addEventListener('DOMContentLoaded', function () {
                 // Session storage used so the value "yes" does not persist
                 //+ if the extension is closed and then reopened
                 let alerted = sessionStorage.getItem(text) || null;
-                // If the alert has not been shown yet
-                //+ (and therefore is set to null), the alert message will appear
-                //+ and the key's value is set to "yes"
                 if(alerted != "yes") {
-                    alert("The Latex " + text + " will be removed from the output.");
+                    alert("The LaTeX " + text + " will be removed from the output.");
                     sessionStorage.setItem(text, "yes");
                 }
             }
@@ -118,30 +106,25 @@ document.addEventListener('DOMContentLoaded', function () {
         checkInput("\\write18");
     });
 
-
     // SENDS MESSAGE TO latex_transport.js,
     // QUERYING IF DOUBLE CLICK EDIT HAS BEEN USED.
     // IF YES, DATA IS SENT BACK AND STORED IN retrieved_latex,
     Browser.runtime.sendMessage({ type: 'latex_retrieve' }, function(retrieved_latex) {
         // IF NO DOUBLE CLICK EDIT
-        if (retrieved_latex == 'undefined')
-        {
+        if (retrieved_latex == 'undefined'){
             // IF NO PREVIOUS CODE WAS ENTERED
-            if (localStorage.getItem("text") == null)
-            {
-                input.value = 'Enter code here';
+            if (localStorage.getItem("text") == null){
+                input.value = 'ax^2 + bx + c';
             }
-            else    // CODE WAS PREVIOUSLY ENTERED
-            {
+            else {
+                // CODE WAS PREVIOUSLY ENTERED
                 input.value = localStorage.getItem("text");
             }
         }
-        else    // DOUBLE CLICK EDIT WAS USED
-        {
+        else {   // DOUBLE CLICK EDIT WAS USED
             input.value = retrieved_latex;
             localStorage.setItem("text", retrieved_latex);
         }
-
         document.getElementById("code").select();
     });
 
@@ -151,7 +134,6 @@ document.addEventListener('DOMContentLoaded', function () {
     // Reference for hexToRgb function:
     // https://stackoverflow.com/questions/5623838/rgb-to-hex-and-hex-to-rgb
     color.addEventListener('change', (e) => {
-        //Takes hex (color.value) and returns the red, green, and blue rgb values
         function hexToRgb(hex) {
             var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
             return result ? {
@@ -165,25 +147,21 @@ document.addEventListener('DOMContentLoaded', function () {
         greenColor = hexToRgb(color.value).g;
         blueColor = hexToRgb(color.value).b;
 
-        // Set value of rgb input boxes to the rgb values selected in the color picker
         document.getElementById('red').value = redColor;
         document.getElementById('green').value = greenColor;
         document.getElementById('blue').value = blueColor;
 
-        // Make the values selected from color picker persistent
         localStorage.setItem("red", redColor);
         localStorage.setItem("green", greenColor);
         localStorage.setItem("blue", blueColor);
     });
 
-    // Adjusts color in the color picker box accroding to rgb value entered
     function colorPickerChange(){
 
         let red = document.getElementById('red');
         let green = document.getElementById('green');
         let blue = document.getElementById('blue');
 
-        // Changes rgb decimal values to hex value
         function rgbToHex (rgb) {
             var hex = Number(rgb).toString(16);
             if (hex.length < 2) {
@@ -192,7 +170,6 @@ document.addEventListener('DOMContentLoaded', function () {
             return hex;
         };
 
-        // Combines red, green, blue hex values to get hex color value
         function fullColorHex (r,g,b) {
             var red = rgbToHex(r);
             var green = rgbToHex(g);
@@ -222,7 +199,6 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     });
 
-
     // LISTENS FOR KEY COMBO TO CONVERT IMAGE -- SEE BELOW
     document.body.addEventListener("keydown", function(e) {
         e = e || window.event;
@@ -230,27 +206,21 @@ document.addEventListener('DOMContentLoaded', function () {
         // Ctrl+Enter for Windows and Linux -- additional Command+Enter option for Mac
         if ((e.ctrlKey || e.metaKey) && e.key === "Enter")
         {
-            // GET ELEMENT THAT IS IN FOCUS
             let focusElement = document.activeElement;
 
-            // IN CASE ELEMENT IN FOCUS IS A TEXT BOX
-            // BLURS IT SO STRANGE FIREFOX Ctrl+Enter AUTO-COPY BUG DOES NOT OCCUR
+            // In case element in focus is a text box, blurs it so a strange
+            // Firefox auto-copy bug doesn't occur.
             focusElement.blur();
 
-            // SIMULATES A CLICK ON THE CONVERT BUTTON
             document.getElementById('submitButton').click();
-
-            // PUTS ELEMENT THAT WAS PREVIOUSLY IN FOCUS BACK IN FOCUS
             focusElement.focus();
         }
     });
 
-
-    let form = document.getElementById('form');
-
     // Submit - occurs when the user presses the "Submit" button or Ctrl+Enter
     // Should copy image to clipboard
-    form.addEventListener('submit', function (e) {
+    let form = document.getElementById('form');
+    form.addEventListener('submit', function (e){
         e.preventDefault();
         // Get values from user configuration
         let font = document.getElementById('font');
@@ -258,10 +228,7 @@ document.addEventListener('DOMContentLoaded', function () {
         let colour = document.getElementById('color');
         let latex = e.target.children.code.value;
 
-        // Prepares latex input for url, and properly escape forward slashes.
         latex = encodeURIComponent(latex.replace(/\//g, '\\slash'));
-
-        // All session storage info cleared (user will be alerted if certain text is entered per function checkInput())
         sessionStorage.clear();
 
         // Set URL using configuration options
@@ -269,16 +236,14 @@ document.addEventListener('DOMContentLoaded', function () {
             "&c=" + colour.value.slice(1) +
             "&f=" + font.value;
 
-        let value = server + 'compile/' + latex + dataString;
-
+        let value = server + 'image/' + latex + dataString;
         var xhr = new XMLHttpRequest();
 
         xhr.onreadystatechange = function () {
             // If the server has responded
             if (xhr.readyState == 4) {
-                // REMOVE PREVIOUS IMAGE BEFORE ADDING NEW ONE, CHECKING IF IT EXISTS FIRST
                 let imageExists = !!document.getElementById('latex_image');
-                if (imageExists) {
+                if (imageExists){
                     document.getElementById('latex_image').remove();
                 }
 
@@ -304,12 +269,10 @@ document.addEventListener('DOMContentLoaded', function () {
                 sel.removeAllRanges();
             }
 
-            //If the server has responded with an empty message and the browser is chrome
             if(xhr.readyState == 4 && xhr.status == 0 && Browser == chrome){
                 alert("Please ensure the latest version of Tex Math Here is installed. If it is, try again in a few minutes.")
             }
 
-            // Triggers xhr.status = 0 if it hasn't proccessed already or hasn't already arrived at that state
             xhr.timeout = 10000; // 10 seconds
 
             // After 10 seconds have passed
